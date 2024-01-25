3d538dad32a796287b085a3a464e90b87f9d3c2c
athulyasab <146376174+athulyasab@users.noreply.github.com>
2024-01-25 16:38:59 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,6 +1,6 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 7,
     center: { lat: 50.327849,  lng: -85.319336 },
   });
   // Create an array of alphabetical characters used to label the markers.
